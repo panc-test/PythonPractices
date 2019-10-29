3548b6327fb5b7067ffa0005880fa2f4700583a9
panchao <chao.pan@dhc.com.cn>
2019-10-29 10:33:52 +0800

    20191029

--- 201910/_python%.py
+++ 201910/_python%.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
-#转义字符：格式化输出字符串
 
-price=100
+#转义字符：格式化输出字符串
+price="100\n"
 print("price=%s" %price)
-print("price的ASCII码=%c" %price)
+
+# \：标识一行未结束强制换到下一行续写
+s='Hello\nCharlie\nGood\nMorning\n'
+print(s)
+
+# \t:水平制表符
+s2 = '商品名\t单价\t数量\t总价'
+print(s2)
