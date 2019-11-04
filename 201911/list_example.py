5623623891f53d01a49d405f9d555e84a8391e76
panchao <chao.pan@dhc.com.cn>
2019-11-04 16:16:41 +0800

    20191104

--- 201911/list_example.py
+++ 201911/list_example.py
@@ -2,21 +2,20 @@
 # -*- coding:utf-8 -*-
 
 #（1）创建列表
-
 #方法1：
 list1=["c.biancheng.net" , 1 , [2,3,4] , 3.0]   #列表可以存储整数、实数、字符串、列表、元组等任何类型的数据，并且和数组不同的是，在同一个列表中元素的类型也可以不同
 print("list1=",list1)
-
 #方法2：
-range1=range(3,8)
+range1=range(3,5)
 list2=list(range1)
 print("list2=",list2)
 
-#访问列表
-print(list1[0:3])
+#（2）访问列表
+print(list1[0:3])    #切片
 
-#删除列表：实际开始时，del 语句不常用，因为 Python 自带的垃圾回收机制会自动销毁不用的列表，所以即使开发者不手动将其删除，Python 也会自动将其回收。
+#（3)删除列表：实际开始时，del 语句不常用，因为 Python 自带的垃圾回收机制会自动销毁不用的列表，所以即使开发者不手动将其删除，Python 也会自动将其回收。
 name = name = ["C语言中文网","http://c.biancheng.net"]
 print(name)
 del name
+
 
---
+++ 201911/list_methods.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+
+#（4）列表的常见方法
+
+#append() 将追加的列表视为一个元素，直接添加到列表中，只能添加一个元素
+list1.append(list2)
+print('list1=',list1)
+
+#extend()将一个列表中每个元素分别添加到另一个列表中，可以添加多个元素
+list1.extend(list2)
+print('list1=',list1)
