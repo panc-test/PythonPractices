1f3003075db15b267d216e13786eb02a59c0abb7
panchao <chao.pan@dhc.com.cn>
2019-11-11 15:14:16 +0800

    20191111

---
+++ 201911/function/function_dir.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+
+str=''
+list=[1,2,'a']
+tuple=(1,'a')
+dict={'one':1,'two':2,'three':3}
+set={'a',5,(2,6)}
+
+print('字符串的方法：',dir(str))
+print('列表的方法：',dir(list))
+print('元组的方法：',dir(tuple))
+print('字典的方法：',dir(dict))
+print('集合的方法：',dir(set))
