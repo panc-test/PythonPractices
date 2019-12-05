356bae1fe8dd8c812f60e00e6bb47d5782e38f85
panc-test <1976085712@qq.com>
2019-12-05 22:52:18 +0800

    '20191205'

--- 201911/string_methods/string_split.py
+++ 201911/string_methods/string_split.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
 
-#split() 方法可以实现将一个字符串按照指定的分隔符切分成多个子串，这些子串会被保存到列表中（不包含分隔符），作为方法的返回值反馈回来。
-# 语法：str.split(sep,maxsplit)
+#split() 方法通过指定分隔符对字符串进行切片
+# 语法：str.split(sep, count)
+# sep -- 可选参数，指定的分隔符，默认为所有的空字符，包括空格、换行(\n)、制表符(\t)等。
+# count -- 可选参数，分割次数，默认为分隔符在字符串中出现的总次数。
 
-str = "C语言中文网 >>> c.biancheng.net"
-a=str.split()
-print(type(a))
+str = "C语言\n中文网 >>> c.biancheng.net"
+a=str.split()   # 以空格为分隔符，包含 \n
 print(a)
