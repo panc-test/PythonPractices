7ba68db5e9b1eba17f880eb872cb30dbfcedd56a
panc-test <1976085712@qq.com>
2020-03-23 14:03:06 +0800

    '20200323'

--- requests/requests_study01.py
+++ requests/requests_study01.py
@@ -4,12 +4,12 @@ requests-接口请求模块
 
 import requests
 
-
 url='http://39.107.96.138:3000/api/v1/topics'
 #参数是字典形式
 params={'page':1,'tab':'share','limit':1,'mdrender':'false'}
 
 r=requests.get(url,params)
+print(r)
 response=r.json()
 
 print(r.json())
