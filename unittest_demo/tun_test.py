e464c8c9f139ee8345ca0b1cd69da582d038beea
panchao <chao.pan@dhc.com.cn>
2020-09-05 18:31:18 +0800

    '2020-09-05'

---
+++ unittest_demo/tun_test.py
@@ -0,0 +1,37 @@
+"""
+生成测试报告的几种方式
+
+"""
+
+import unittest
+from HTMLTestRunner import HTMLTestRunner
+from BeautifulReport import BeautifulReport
+
+
+def suite():
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()       #加载器
+    tests = loader.discover(start_dir='./testcases',pattern='test*.py')
+    suite.addTests(tests)
+    return suite
+
+# TextTestRunner()
+# if __name__ == '__main__':
+#     suite = suite()
+#     with open(file='./report/log.txt',mode='a') as file:
+#         runner = unittest.TextTestRunner(verbosity=2,stream=file)
+#         runner.run(suite)
+
+
+# HTMLTestRunner()
+# if __name__ == '__main__':
+#     suite=suite()
+#     with open(file='./report/report.html',mode='wb') as file:
+#         runner=HTMLTestRunner.HTMLTestRunner(stream=file,title='测试报告',verbosity=2)
+#         runner.run(suite)
+
+
+if __name__ == '__main__':
+    suite=suite()
+    result=BeautifulReport(suite)
+    result.report(report_dir='./report',filename='breport.html',description='注册和登录功能测试报告')
