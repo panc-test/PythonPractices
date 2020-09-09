8214d94a5faf0ca62a7a416848687ec7076a856f
panchao <chao.pan@dhc.com.cn>
2020-09-09 19:17:19 +0800

    '2020-09-09'

--- zipfile/zip_study02.py
+++ zipfile/zip_study02.py
@@ -6,7 +6,7 @@ from zipfile import ZipFile
 
 
 file_name='./new.zip'
-file_list=['Snipaste.png','test.py']
+file_list=['Snipaste.png','test_fun.py']
 with ZipFile(file=file_name,mode='w') as myzip:    #注意w会删除压缩包内已有的文件
     for f in file_list:
         myzip.write(f)
