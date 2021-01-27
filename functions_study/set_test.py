9e3b1aba7b76afd8c1a90212f2e50b028dd989c6
panc-test <1976085712@qq.com>
2021-01-27 14:22:46 +0800

    2021-01-27

---
+++ functions_study/set_test.py
@@ -0,0 +1,10 @@
+"""
+set() python内置的去重函数，返回一个无序的集合
+
+"""
+
+list = [1,2,2,2,5,8]
+str = 'aaabbbccc'
+
+print(set(list),type(set(list)))
+print(set(str))
--- iter_study/iter_test02.py
+++ iter_study/iter_test02.py
@@ -1,20 +1,34 @@
 """
-可迭代的对象 :只有__iter__方法,没有__next__方法。
+迭代器：
+1、如果在一个类中定义__iter__方法和__next__方法，那么这个类的实例就是一个迭代器。
+2、__iter__方法需要返回一个迭代器，所以就返回对象本身即可(也就是self)。
+3、当对象每迭代一次时，就会调用迭代器中的另外一个特殊成员方法__next__。该方法需要返回当前迭代的结果，超出边界值会抛出StopIteration异常。
 
 """
-#自定义一个可迭代的对象
-class Iter1:
-  def __init__(self, text):
-    self.text = text
 
+#自定义可迭代的类
+class MyIter:
+
+  def __init__(self, start, end):
+    self.count = start
+    self.end = end
+  #返回一个迭代器
   def __iter__(self):
-    return iter(self.text)
+    return self
+  #定义迭代规则
+  def __next__(self):
+    if self.count < self.end:
+      result = self.count
+      self.count += 1
+      return result
+    else:
+      raise StopIteration
 
-#实例化一个可迭代对象
-iter1 = Iter1("hello")
-print(type(iter1))
+#实例化一个迭代器
+it = MyIter(1, 10)
+print(type(it))
 
 #for 循环遍历迭代器
-for s in iter1:
-  print(s)
+for i in it:
+  print(i)
 
