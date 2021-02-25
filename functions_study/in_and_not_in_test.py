96cef65ac912ac9b31b4f9233b66925af29a6f08
panc-test <1976085712@qq.com>
2021-02-25 19:03:16 +0800

    2021-02-25

---
+++ functions_study/in_and_not_in_test.py
@@ -0,0 +1,11 @@
+"""
+关键字 in 和 not in 判断特定的值是否存在列表中
+
+"""
+
+
+letters = ['A','B','C','D','E','F','G']
+if 'A' in letters:
+    print('A'+' exists')
+if 'h' not in letters:
+    print('h'+' not exists')
