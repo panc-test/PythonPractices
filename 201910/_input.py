50b10c44ab0afbf9d6e0a1eba11eab70f506aae5
panc-test <1976085712@qq.com>
2021-02-26 16:53:04 +0800

    ''--

--- 201910/_input.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python 
-# -*- coding:utf-8 -*-
-'''
-input() 函数用于获取用户输入的内容。由于 input() 函数总会将用户输入的内容放入字符串中，因此用户可以输入任何内容，input() 函数总是返回一个字符串。
-'''
-
-msg=input("请输入你的值：")
-print(msg)
-print(type(msg))
--- 201910/key_words.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python 
-# -*- coding:utf-8 -*-
-#python关键字
-
-import keyword
-print("python关键字：",keyword.kwlist)
