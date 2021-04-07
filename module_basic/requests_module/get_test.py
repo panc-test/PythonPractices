cfb241b429f8a7bcc48379be91a76b995a63c1e1
unknown <057776@chinatopcredit.com>
2021-04-07 15:22:39 +0800

    '2021-04-07'

---
+++ module_basic/requests_module/get_test.py
@@ -0,0 +1,21 @@
+'''
+get请求
+1、功能查询数据，对数据库执行查询操作
+2、请求参数在url上可以看到
+
+'''
+import requests
+
+#接口请求地址
+url = 'http://hn216.api.yesapi.cn/'
+#请求参数（不能有空额，数据类型）
+params={
+    's':'App.Common_Phone.QueryLocation',
+    'app_key':'363CF6F2AC7C0290EE53329F28ADFD68',
+    'phone':'15221738510',
+    # 'return_data':'0',
+    # 'sign':'6A64BDEA3624F6E36F78FFD4DCA8768B'
+}
+r=requests.get(url,params)
+assert r.status_code == 200
+print(r.json())
--- module_basic/requests_module/post_test.py
+++ module_basic/requests_module/post_test.py
@@ -1,14 +1,14 @@
 '''
-requests_study-post请求
+post请求
 1、功能提交数据，对数据库执行更新操作
 2、请求参数在body中，请求的格式多样性，可以是表单格式，json格式
 3、比get请求的数据长度长
-'''
 
-import requests_study
-base_url='http://49.233.108.117:3000/api/v1'
-token='80b11be8-cb8f-470a-9f7a-e415a8f1b3be'
-#发布话题
+'''
+import requests
+
+url = 'http://apis.juhe.cn/mobile/get'
+token = '80b11be8-cb8f-470a-9f7a-e415a8f1b3be'
 body={
     'accesstoken':token,
     'title':'panc-xxxxxxxxx',
@@ -16,8 +16,8 @@ body={
     'tab':'ask'
 }
 #post请求的参数可以有2种数据格式
-r=requests_study.post(url=base_url + '/topics', data=None, json=body)
+r=requests.post(url=url, data=None, json=body)
 
-print('响应码：',r.status_code)
-print('响应体：',r.json())
+assert r.status_code == 200
+print(r.json())
 
