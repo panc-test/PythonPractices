230feb7d18db91e43bffa480a7e5314d82a3dadb
panc-test <1976085712@qq.com>
2021-09-27 14:43:12 +0800

    2021-09-27

--- python_basics/format_test.py
+++ python_basics/format_test.py
@@ -1,20 +1,29 @@
 """
-格式化字符：
-1.使用占位符 % 格式化
-2.使用.format()方法格式化  ----python2.6
-3.f-Strings表达式,可以解析任意类型的数据，运行的时候渲染，性能比%，.format()更好。python3.6
+格式化：
+1.使用占位符 % 格式化，最初的
+2.使用str.format()格式化，python2.6版本引入
+3.f-strings表达式,可以解析任意类型的数据，运行的时候渲染，性能比%，.format()更好。python3.6版本引入
 
 """
-# f"xxx" 表达式用法
 name = 'peter'
-age = 24
-print(f"{name} is {age} years old")
-print(F"{name} is {age} years old")
-# 解析任意表达式
+age = 20
+# %s 格式化字符串，%d 格式化整数
+print('%s is %d years old' % (name, age))
+
+# str.format() 三种写法,不限制参数类型，数量
+print('{} is {} years old'.format(name, age))
+print('{1} is {1} years old'.format(name, age))
+print('{name} is {name} years old'.format(name='test', age=22))
+
+# f-string表达式基本用法
+print(f'{name} is {age} years old')
+print(F'{name} is {age} years old')
+
+# 解析表达式
 print(f'{1 + 2}')
-print(f'{2 * 3}')
-# 解析字符串方法
+print(f'{2 * 4}')
+# 解析内置函数
 print(f'{name.upper()}')
-# 解析列表，元组，字典
-list = ['hello','world']
+# 解析基本数据类型的属性方法
+list = ['hello', 'world']
 print(f'{list[0]}')
